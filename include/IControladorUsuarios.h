49707f9d1a75f39a84f241c4c1df0d8d0748b692
Belisk <blackbelisk@gmail.com>
2023-05-29 19:51:52 -0300

    Algunas cosas

---
+++ include/IControladorUsuarios.h
@@ -0,0 +1,16 @@
+#ifndef _CLASSICONTROLADORUSUARIOS_H_
+#define _CLASSICONTROLADORUSUARIOS_H_
+
+#include <iostream>
+#include <set>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class IControladorUsuarios{
+    public:
+    private:
+};
+
+#endif
---
+++ include/controladorUsuarios.h
@@ -0,0 +1,16 @@
+#ifndef _CLASSCONTROLADORUSUARIOS_H_
+#define _CLASSCONTROLADORUSUARIOS_H_
+
+#include <iostream>
+#include <set>
+#include <vector>
+#include <string>
+#include <IControladorUsuarios.h>
+using namespace std;
+
+class controladorUsuarios:IControladorUsuarios{
+    public:
+    private:
+};
+    
+#endif
