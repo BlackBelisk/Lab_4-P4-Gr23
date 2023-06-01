24df14e642cfb205d16c443c0e800f07d17dad11
Belisk <blackbelisk@gmail.com>
2023-06-01 16:26:28 -0300

    DDD

--- include/controladorUsuarios.h
+++ include/controladorUsuarios.h
@@ -5,15 +5,47 @@
 #include <set>
 #include <vector>
 #include <string>
+#include <map>
 #include <IControladorUsuarios.h>
+#include "usuario.h"
+#include "estudiante.h"
+#include "profesor.h"
+
 using namespace std;
 
 class ControladorUsuarios: public IControladorUsuarios{
-    public:
-        Estudiante* encontrarEstudiante(string) override;
-
-        static ControladorUsuarios& getInstance();
     private:
+        map<string, Usuario*> usuarios;
+        map<string, Profesor*> profesores;
+        map<string, Estudiante*> estudiantes;
+        set<Idioma*> especializacionesProfesor;
+    public:
+        static ControladorUsuarios& getInstance();
+
+        void ingresarUsuario(string, string, string, string);
+        void ingresarDatosEstudiante(string, Fecha);
+        void ingresarInstituto(string);
+        void agregarEspecializacion(Idioma);
+
+        void confirmarAltaUsuario();
+        void confirmarAltaEstudiante();
+        void confirmarAltaProfesor();
+
+        void nuevoEstudiante(string, string, string, string, Fecha, string);
+        void nuevoProfesor(string, string, string, string, string, set<Idioma*>);
+
+        set<Usuario*> obtenerUsuarios();
+        set<Estudiante*> obtenerEstudiantes();
+        set<Profesor*> obtenerProfesores();
+
+        Usuario* encontrarUsuario(string);
+        Profesor* encontrarProfesor(string);
+        Estudiante* encontrarEstudiante(string);
+
+        void elegirIdiomaProfesor(Idioma*);
+
+        Notificacion consultarNotificaciones(string);
+
 };
     
 #endif
