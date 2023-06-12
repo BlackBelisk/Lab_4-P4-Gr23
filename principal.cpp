ef34ac455a7dee7a08df300ec53c67c9c98c9f85
TerevintoMartin <101746971+TerevintoMartin@users.noreply.github.com>
2023-06-12 19:33:36 -0300

    Update principal.cpp
    agregarCurso (version 1.0)

--- principal.cpp
+++ principal.cpp
@@ -177,6 +177,135 @@ void consultarIdiomas(){
 } 
 
 void altaCurso(){
+    Factory * factory = Factory::getInstance();
+    IControladorCursos* cc = factory->getIControladorCursos();
+
+    //Se crea el curso
+    string nombreCurso;
+    cout << "Ingrese el nombre del curso: " << endl;
+    cin >> nombreCurso;
+
+    string descripcionCurso;
+    cout << "Ingrese la descripcion del curso: " << endl;
+    cin >> nombreCurso;
+
+    dif dificultadCurso;
+    do
+        {
+            cout << "Seleccione la dificultad del curso: " << endl << "0. Principiante" << endl << "1. Intermedio" << endl << "2. Avanzado" << endl;
+            cin >> dificultadCurso;
+            if (dificultadCurso < 0 || dificultadCurso > 2)
+            {
+                cout << "Numero de dificultad invalido. Intente nuevamente." << endl;
+            }
+            
+        } while (dificultadCurso < 0 || dificultadCurso > 2);
+    
+    //CREACION DEL CURSO
+    cc->crearCurso(nombreCurso, descripcionCurso, dificultadCurso);
+
+
+    //Selecciona el profesor que dictara el curso
+    IControladorUsuarios* cUsers = factory->getIControladorUsuarios();
+    list<DataProfesor> profesores = cUsers->obtenerProfesores();
+    imprimirListaDataProfesores(profesores);
+    int profe;
+    do
+        {
+            cout << "Seleccione el profesor que dictara el curso ingresando el numero: " << endl;
+            cin >> profe;
+            if (profe < 0 || profe > profesores.size())
+            {
+                cout << "Numero de profesor invalido. Intente nuevamente." << endl;
+            }
+            
+        } while (profe < 0 || profe > profesores.size());
+    auto it = profesores.begin();
+    advance(it, profe - 1);
+
+    //AQUI
+    cc->encontrarCurso(nombreCurso)->elegirProfesor(it->getNick());
+
+
+    //Selecciona el idioma del curso a partir del profesor seleccionado
+    IControladorIdiomas* ci = factory->getIControladorIdiomas();
+    list<DataIdioma> idprof = profesor.listarIdiomasProfesor();
+    imprimirListaDataIdiomas(idprof);
+    int idioma;
+    do
+        {
+            cout << "Seleccione el idioma del curso ingresando el numero: " << endl;
+            cin >> idioma;
+            if (idioma < 0 || idioma > idprof.size())
+            {
+                cout << "Numero de idioma invalido. Intente nuevamente." << endl;
+            }
+            
+        } while (idioma < 0 || idioma > idprof.size());
+    auto it2 = idprof.begin();
+    advance(it2, idioma - 1);
+
+    //AQUI
+    cc->encontrarCurso(nombreCurso)->elegirIdiomaProfesor(it2->getNombre());
+
+
+    //Selecciona las Previas que contiene el curso (puede contener o no)
+    int seleccionador;
+    do
+        {
+            cout << "Si el curso contiene previas presione 1, sino seleccione 0" << endl;
+            cin >> seleccionador;
+            if (seleccionador != 0 || seleccionador != 1)
+            {
+                cout << "Accion Incorrecta. Intente nuevamente." << endl;
+            }
+            
+        } while (seleccionador != 0 || seleccionador != 1);
+
+    if (seleccionador = 1){
+        //Agregar previas
+        list<DataCurso> cursos = cc->listarCursosHab();
+        imprimirListaCursos(cursos);
+        int curso;
+
+        //Bucle para agregar tantas previas como quiera
+        do
+        {
+            do
+            {
+                cout << "Seleccione el curso ingresando el numero: " << endl;
+                cin >> curso;
+                if (curso < 0 || curso > cursos.size())
+                {
+                    cout << "Numero de curso invalido. Intente nuevamente." << endl;
+                }
+            
+            } while (curso < 0 || curso > cursos.size());
+
+            //Agrega la previa seleccionada
+            auto it3 = cursos.begin();
+            advance(it3, curso - 1);
+
+            //AQUI
+            cc->encontrarCurso(nombreCurso)->agregarPrevia(it3->getNomCurso());
+
+            //Seguir agregando previas??
+            do
+            {
+                cout << "Para seguir seleccionando previas presione 1, para dejar de seleccionar presione 0" << endl;
+                cin >> seleccionador;
+                if (seleccionador != 0 || seleccionador != 1)
+                {
+                    cout << "Accion Incorrecta. Intente nuevamente." << endl;
+                    }
+            
+            } while (seleccionador != 0 || seleccionador != 1);
+            
+        } while (seleccionador != 0);
+    }
+
+
+    //Agregar Lecciones y Ejercicios al Curso (puede no agregar)
 
 }
 
